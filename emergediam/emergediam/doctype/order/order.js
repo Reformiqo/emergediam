a876b3738f67fa8102e025055503bff7699a0808
khanreformiqo <momdou.khan@reformiqo.com>
2025-04-12 08:33:49 +0000

    update

--- emergediam/emergediam/doctype/order/order.js
+++ emergediam/emergediam/doctype/order/order.js
@@ -30,6 +30,14 @@ frappe.ui.form.on('Order', {
 
         // Hide all pricing tables
         update_pricing_tables_visibility(frm);
+        
+        // Hide all order summary tables
+        update_order_summary_tables_visibility(frm);
+        
+        // Set shape back to select type if it was changed to data type
+        frm.set_df_property("shape", "fieldtype", "Select");
+        frm.set_df_property("shape", "options", "Round\nOval\nEmerald\nPear\nMarquise\nPrincess\nRadiant\nAny Other\nTrillion\nTrapezoid\nTapered Baguette\nBaguette");
+        frm.refresh_field("shape");
     },
     
     // Button handlers for each shape
@@ -40,79 +48,130 @@ frappe.ui.form.on('Order', {
         frm.set_df_property("order_by_mm_table", "hidden", 0);
         frm.set_df_property("order_by_pointer_table", "hidden", 0);
         frm.set_df_property("order_by_sieve_table", "hidden", 0);
+        // Set shape field value
+        frm.set_value("shape", "Round");
     },
     
     oval: function(frm) {
         frm.events.hide_all_tables(frm);
         frm.set_df_property("oval_table", "hidden", 0);
+        // Set shape field value
+        frm.set_value("shape", "Oval");
     },
     
     emerald: function(frm) {
         frm.events.hide_all_tables(frm);
         frm.set_df_property("emerald_table", "hidden", 0);
+        // Set shape field value
+        frm.set_value("shape", "Emerald");
     },
     
     pear: function(frm) {
         frm.events.hide_all_tables(frm);
         frm.set_df_property("pear_table", "hidden", 0);
+        // Set shape field value
+        frm.set_value("shape", "Pear");
     },
     
     marquise: function(frm) {
         frm.events.hide_all_tables(frm);
         frm.set_df_property("marquise_table", "hidden", 0);
+        // Set shape field value
+        frm.set_value("shape", "Marquise");
     },
     
     princess: function(frm) {
         frm.events.hide_all_tables(frm);
         frm.set_df_property("princess_table", "hidden", 0);
+        // Set shape field value
+        frm.set_value("shape", "Princess");
     },
     
     radiant: function(frm) {
         frm.events.hide_all_tables(frm);
         frm.set_df_property("radiant_table", "hidden", 0);
+        // Set shape field value
+        frm.set_value("shape", "Radiant");
     },
     any_other: function(frm) {
         frm.events.hide_all_tables(frm);
         frm.set_df_property("any_other_table", "hidden", 0);
-        frm.set_df_property("shape", "hidden", 0);
+        // Change the field type to data for custom input
+        frm.set_df_property("shape", "fieldtype", "Data");
+        frm.set_df_property("shape", "options", "");
+        frm.refresh_field("shape");
+        // Clear the shape field for user input
+        frm.set_value("shape", "");
     },
 	trillion: function(frm) {
         frm.events.hide_all_tables(frm);
         frm.set_df_property("trillion_table", "hidden", 0);
+        // Set shape field value
+        frm.set_value("shape", "Trillion");
     },
 	trapezoid: function(frm) {
         frm.events.hide_all_tables(frm);
         frm.set_df_property("trapezoid_table", "hidden", 0);
+        // Set shape field value
+        frm.set_value("shape", "Trapezoid");
     },
 	tapered_baguette: function(frm) {
         frm.events.hide_all_tables(frm);
         frm.set_df_property("tapered_baguette_table", "hidden", 0);
+        // Set shape field value
+        frm.set_value("shape", "Tapered Baguette");
     },
 	baguette: function(frm) {
         frm.events.hide_all_tables(frm);
         frm.set_df_property("baguette_table", "hidden", 0);
+        // Set shape field value
+        frm.set_value("shape", "Baguette");
     },
     refresh: function(frm) {
         // Hide all tables on initial load
         frm.events.hide_all_tables(frm);
-        frm.set_df_property("shape", "hidden", 1);
-    }
+        // Make shape field visible by default, but keep as Select type
+        frm.set_df_property("shape", "hidden", 0);
+        
+        // Update visibility of pricing and order summary tables
+        update_pricing_tables_visibility(frm);
+        update_order_summary_tables_visibility(frm);
+    },
+    after_save: function(frm) {
+        // Update both pricing and order summary tables after saving
+        update_pricing_tables_visibility(frm);
+        update_order_summary_tables_visibility(frm);
+    },
+    shape: function(frm) {
+        // Get the selected shape value
+        const selectedShape = frm.doc.shape;
+        
+        // Only proceed if there's a value and it's not coming from "Any Other"
+        if (selectedShape && frm.get_field("shape").df.fieldtype === "\d") {
+            // Convert the shape name to lowercase and remove spaces for the function name
+            const functionName = selectedShape.toLowerCase().replace(/\s+/g, '_');
+            
+            // Call the corresponding function if it exists
+            if (typeof frm.events[functionName] === 'function') {
+                frm.events[functionName](frm);
+            }
+        }
+    },
 });
 
 // Function to update pricing tables visibility
 function update_pricing_tables_visibility(frm) {
     const pricing_tables = [
         "emerald_pricing_table",
+		"order_by_pointer_pricing_table",
+        "order_by_sieve_pricing_table",
+		"order_by_mm_pricing_table",
         "oval_pricing_table",
         "pear_pricing_table",
         "marquise_pricing_table",
         "princess_pricing_table",
         "radiant_pricing_table",
         "any_other_pricing_table",
-        "trillion_pricing_table",
-        "order_by_mm_pricing_table",
-        "order_by_pointer_pricing_table",
-        "order_by_sieve_pricing_table",
         "trillion_pricing_table",
         "trapezoid_pricing_table",
         "tapered_baguette_pricing_table",
@@ -121,6 +180,33 @@ function update_pricing_tables_visibility(frm) {
 
     // Check each pricing table
     pricing_tables.forEach(table => {
+        const tableData = frm.doc[table] || [];
+        // Hide table if empty, show if has data
+        frm.set_df_property(table, "hidden", tableData.length === 0);
+    });
+}
+
+// Function to update order summary tables visibility
+function update_order_summary_tables_visibility(frm) {
+    const order_summary_tables = [
+        "emerald_order_summary",
+        "oval_order_summary",
+        "pear_order_summary",
+        "marquise_order_summary",
+        "princess_order_summary",
+        "radiant_order_summary",
+        "any_other_order_summary",
+        "trillion_order_summary",
+        "order_by_mm_order_summary",
+        "order_by_pointer_order_summary",
+        "order_by_sieve_order_summary",
+        "trapezoid_order_summary",
+        "tapered_baguette_order_summary",
+        "baguette_order_summary",
+    ];
+
+    // Check each order summary table
+    order_summary_tables.forEach(table => {
         const tableData = frm.doc[table] || [];
         // Hide table if empty, show if has data
         frm.set_df_property(table, "hidden", tableData.length === 0);
@@ -144,6 +230,8 @@ function calulate_pending_amount(frm, cdt, cdn) {
         let amount = flt(row.quantity_cts) - flt(row.ready) - flt(row.factory);
         frappe.model.set_value(cdt, cdn, 'pending', amount);
     }
+    // Update order summary tables visibility after calculation
+    update_order_summary_tables_visibility(frm);
 }
 
 function generate_order_id(frm, cdt, cdn, table) {
@@ -206,9 +294,13 @@ frappe.ui.form.on('Oval', {
 	oval_table_add: function(frm, cdt, cdn) {
 		// auto set the order id 
 		generate_order_id(frm, cdt, cdn, 'oval_table');
-		
-		
+		// Update order summary tables visibility
+		update_order_summary_tables_visibility(frm);
 	},
+	oval_table_remove: function(frm) {
+        // Update order summary tables visibility
+        update_order_summary_tables_visibility(frm);
+    },
 	order_type: function(frm, cdt, cdn) {
 		generate_order_id(frm, cdt, cdn, 'oval_table');
 		frm.refresh_field('oval_table');
@@ -622,6 +714,10 @@ frappe.ui.form.on('Pricing', {
     quantity_cts: function(frm, cdt, cdn) {
         calculate_amount(frm, cdt, cdn);
     },
+    pricing_add: function(frm, cdt, cdn) {
+        // Update visibility when a row is added
+        update_pricing_tables_visibility(frm);
+    },
     // Add handler for row deletion
     pricing_remove: function(frm, cdt, cdn) {
         update_pricing_tables_visibility(frm);
@@ -632,11 +728,19 @@ frappe.ui.form.on('Order Summary', {
     quantity_cts: function(frm, cdt, cdn) {
         calulate_pending_amount(frm, cdt, cdn);
     },
-	ready: function(frm, cdt, cdn) {
-		calulate_pending_amount(frm, cdt, cdn);
-	},
-	factory: function(frm, cdt, cdn) {
-		calulate_pending_amount(frm, cdt, cdn);
-	},
-	
+    ready: function(frm, cdt, cdn) {
+        calulate_pending_amount(frm, cdt, cdn);
+    },
+    factory: function(frm, cdt, cdn) {
+        calulate_pending_amount(frm, cdt, cdn);
+    },
+    // Add handlers for when summary rows are added or removed
+    after_insert: function(frm) {
+        // Update order summary tables visibility when rows are added
+        update_order_summary_tables_visibility(frm);
+    },
+    remove: function(frm) {
+        // Update order summary tables visibility when rows are removed
+        update_order_summary_tables_visibility(frm);
+    }
 });
